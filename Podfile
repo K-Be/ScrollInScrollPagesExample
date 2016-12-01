6dee50c36c85657e879dcfae374e4488723e7ccf
Andrew Romanov <scalli-k-be@ya.ru>
2016-12-01 19:02:58 +0700

    added example of scrollview inside scrollview

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment this line to define a global platform for your project
+
+target 'ScrollInScrollPages' do
+  platform :ios, '8.0'
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
+  #use_frameworks!
+
+  # Pods for BeLikedClientApp
+  pod 'KeepLayout/ObjC', :git => 'https://github.com/iMartinKiss/KeepLayout', :branch => 'master'
+
+  target 'ScrollInScrollPagesTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- ScrollInScrollPages/ViewController.m
+++ ScrollInScrollPages/ViewController.m
@@ -7,23 +7,104 @@
 //
 
 #import "ViewController.h"
+#import <KeepLayout.h>
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UIScrollView* parentScrollView;
+
+@property (nonatomic, strong) UIScrollView* firstScrollChild;
+@property (nonatomic, strong) UIImageView* firstContentImageView;
+
+@property (nonatomic, strong) UIScrollView* secondScrollChild;
+@property (nonatomic, strong) UIImageView* secondContentImageView;
 
 @end
+
+
+@interface ViewController (Private)
+
+- (UIImageView*)_createContentImageWithImageNamed:(NSString*)imageName;
+- (UIScrollView*)_createScrollViewWithContentView:(UIImageView*)contentView;
+
+@end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	_firstContentImageView = [self _createContentImageWithImageNamed:@"1.jpg"];
+	_firstScrollChild = [self _createScrollViewWithContentView:_firstContentImageView];
+	
+	_secondContentImageView = [self _createContentImageWithImageNamed:@"2.jpg"];
+	_secondScrollChild = [self _createScrollViewWithContentView:_secondContentImageView];
+	
+	
+	[_parentScrollView addSubview:_firstScrollChild];
+	[_parentScrollView addSubview:_secondScrollChild];
+	
+	_firstScrollChild.keepSizeTo(self.view).equal = 1.0;
+	_firstScrollChild.keepLeftInset.equal = 0.0;
+	_firstScrollChild.keepVerticalInsets.equal = 0.0;
+	_firstScrollChild.keepRightOffsetTo(_secondScrollChild).equal = 0.0;
+	
+	_secondScrollChild.keepSizeTo(self.view).equal = 1.0;
+	_secondScrollChild.keepVerticalInsets.equal = 0.0;
+	_secondScrollChild.keepRightInset.equal = 0.0;
+	
+	[_firstContentImageView keepCentered];
+	[_secondContentImageView keepCentered];
 }
 
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+	UIView* view = nil;
+	if (scrollView == _firstScrollChild)
+	{
+		view = _firstContentImageView;
+	}
+	else if (scrollView == _secondScrollChild)
+	{
+		view = _secondContentImageView;
+	}
+	
+	return view;
 }
 
+
+@end
+
+
+#pragma mark -
+@implementation ViewController (Private)
+
+- (UIImageView*)_createContentImageWithImageNamed:(NSString*)imageName
+{
+	UIImageView* imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:imageName]];
+	imageView.backgroundColor = [UIColor redColor];
+	imageView.userInteractionEnabled = NO;
+	imageView.contentMode = UIViewContentModeScaleAspectFit;
+	CGRect frame = imageView.frame;
+	frame.size = [UIScreen mainScreen].bounds.size;
+	imageView.frame = frame;
+	
+	return imageView;
+}
+
+
+- (UIScrollView*)_createScrollViewWithContentView:(UIImageView*)contentView
+{
+	UIScrollView* scroll = [[UIScrollView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+	scroll.minimumZoomScale = 0.5;
+	scroll.maximumZoomScale = 3.0;
+	[scroll addSubview:contentView];
+	scroll.delegate = self;
+	
+	return scroll;
+}
 
 @end
